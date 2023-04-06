ffb61088dd3d01040a09417d131f719c8f1e4ce1
Artem <spacenail@gmail.com>
2023-04-06 23:12:37 +0300

    init

---
+++ src/test/java/com/github/spacenail/webmarket/WebmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.spacenail.webmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
